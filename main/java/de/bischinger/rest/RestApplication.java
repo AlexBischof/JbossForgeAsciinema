b7479c4c72a6e5c6f9d80db07196f22b31922ea8
Alexander Bischof <alexanderxbischof@gmail.com>
2015-11-30 01:09:32 +0100

    Initial commit

---
+++ main/java/de/bischinger/rest/RestApplication.java
@@ -0,0 +1,7 @@
+package de.bischinger.rest;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+@ApplicationPath("api")
+public class RestApplication extends Application {
+}
